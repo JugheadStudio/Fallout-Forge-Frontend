37cf5a63705fe6eb4b5ee865468c96a7339566ef
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-03-07 16:20:58 +0200

    add routing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SettlementsComponent } from './components/settlements/settlements.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "landingPage", component: LandingPageComponent},
+  {path: "settlements", component: SettlementsComponent},
+
+];
