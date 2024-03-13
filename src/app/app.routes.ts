1f1669ba6932423460c03fd74dfbebc1f32cadea
DieterR97 <221122@virtualwindow.co.za>
2024-03-13 14:06:07 +0200

    started with login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SettlementsComponent } from './components/settlements/settlements.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {path: "", component: LandingPageComponent},
   {path: "settlements", component: SettlementsComponent},
+  {path: "login", component: LoginComponent},
 
 ];
