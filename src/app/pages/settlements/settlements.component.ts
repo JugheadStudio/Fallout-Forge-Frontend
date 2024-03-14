be3f1a4cb26d68b7f9a19677012d928e58b81393
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-03-14 09:40:31 +0200

    remove navbar from login and registration page

--- src/app/pages/settlements/settlements.component.ts
+++ src/app/pages/settlements/settlements.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
-  selector: 'app-settlements',
-  standalone: true,
-  imports: [],
-  templateUrl: './settlements.component.html',
-  styleUrl: './settlements.component.css'
+    selector: 'app-settlements',
+    standalone: true,
+    templateUrl: './settlements.component.html',
+    styleUrl: './settlements.component.css',
+    imports: [NavbarComponent]
 })
 
 export class SettlementsComponent {
